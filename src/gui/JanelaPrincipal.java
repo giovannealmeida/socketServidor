0118491498f5c77569d951f4590e4e43c1f8c7ab
Giovanne <giovanne.almeida@hotmail.com>
2014-05-08 09:16:20 -0300

    Setando repositorio

---
+++ src/gui/JanelaPrincipal.java
@@ -0,0 +1,88 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.BevelBorder;
+import servidor.Servidor;
+
+/**
+ *
+ * @author Giovanne
+ */
+class JanelaPrincipal extends JFrame implements ActionListener {
+
+    private final int altura = 400;
+    private final int largura = 400;
+
+    private final Servidor servidor = new Servidor();
+
+    private JButton btnStartStop;
+    private ImageIcon imgStart;
+    private ImageIcon imgStop;
+
+    private JPanel panel = new JPanel();
+    private JPanel statusPanel = new JPanel();
+    private JLabel statusLabel = new JLabel("Parado");
+
+    public JanelaPrincipal() {
+
+        imgStart = new ImageIcon(getClass().getResource("ServidorOff.png"));
+        imgStop = new ImageIcon(getClass().getResource("ServidorOn.png"));
+
+        btnStartStop = new JButton(imgStart);
+        btnStartStop.setBorder(BorderFactory.createEmptyBorder());
+        btnStartStop.setContentAreaFilled(false);
+        btnStartStop.setOpaque(false);
+        btnStartStop.addActionListener(this);
+
+//        statusLabel.setHorizontalAlignment(SwingConstants.LEFT);
+        statusPanel.add(statusLabel);
+        statusPanel.setBorder(new BevelBorder(BevelBorder.LOWERED));
+        statusPanel.setPreferredSize(new Dimension(this.getWidth(), 30));
+
+        panel.add(btnStartStop);
+
+        this.setLayout(new BorderLayout());
+        this.add(panel);
+        this.add(statusPanel, BorderLayout.SOUTH);
+        this.setSize(altura, largura);
+        Dimension TamTela = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation((TamTela.width - largura) / 2, (TamTela.height - altura) / 2);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setTitle("Servidor");
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == btnStartStop) {
+            if (servidor.getStatus()) {
+                System.out.println("Parando o servidor...");
+                servidor.kill();
+                System.out.println("Servidor parado com sucesso!");
+                btnStartStop.setIcon(imgStart);
+                statusLabel.setText("Parado");
+                this.dispose();
+            } else {
+                System.out.println("Iniciando o servidor...");
+                servidor.start();
+                System.out.println("Servidor iniciado com sucesso!");
+                btnStartStop.setIcon(imgStop);
+                statusLabel.setText("Iniciado");
+            }
+        }
+    }
+}
---
+++ src/servidor/Servidor.java
@@ -0,0 +1,75 @@
+package servidor;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Giovanne
+ */
+public class Servidor extends Thread{
+
+    private ServerSocket socketServer;
+    public static String arquivoCliente;
+    public String arquivoResposta;
+    public volatile boolean status = false;
+    
+    public void setStatus(boolean status){
+        this.status = status;
+    }
+    
+    public boolean getStatus(){
+        return status;
+    }
+    
+    private File[] achaArquivos(String diretorio) {
+        File dir = new File(diretorio);
+        File fList[] = dir.listFiles();
+        for (int i = 0; i < fList.length; i++) {
+            System.out.println(fList[i]);
+        }
+        return fList;
+    }
+    
+    @Override
+    public void run(){
+        this.status = true;
+        System.out.println("Iniciando thread");
+        try {
+            File arquivosEncontrados [];
+            
+            while (status) {
+                if(socketServer == null || !socketServer.isBound())
+                    socketServer = new ServerSocket(6789);
+                System.out.println("Server ON");
+                Socket conexao = socketServer.accept();
+                System.out.println("Status: "+status);
+                BufferedReader doCliente = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+                DataOutputStream paraCliente = new DataOutputStream(conexao.getOutputStream());
+                arquivoCliente = doCliente.readLine();
+                System.out.println("Comando recebido "+arquivoCliente);
+                if(!status)
+                    return;
+            }
+            
+        } catch (IOException ex) {
+            System.out.println("Socket do servidor fechado enquanto esperava cliente!!");
+        }
+    }
+    
+    public void kill(){
+        this.status = false;
+        try {
+            socketServer.close();
+        } catch (IOException ex) {
+            System.out.println("Socket do servidor fechado!!");
+        }
+    }
+}
